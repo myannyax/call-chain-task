87a11835874d67cfe0a3c4da0fcf62d2528bc225
Maria F <myannyax@gmail.com>
2020-03-30 11:49:17 +0300

    refactor: parseBinaryOperation

--- src/main/kotlin/core/utils/ParseUtils.kt
+++ src/main/kotlin/core/utils/ParseUtils.kt
@@ -13,10 +13,10 @@ fun parseCallChain(callChain: String): CallChain {
 fun parseCall(call: String): Call {
     return when {
         call.startsWith("map{") && call.endsWith("}") -> {
-            MapCall(parseExpression(call.substringAfter("map{").dropLast(1)) ?: throw ParseException())
+            MapCall(parseExpression(call.substringAfter("map{").dropLast(1)))
         }
         call.startsWith("filter{") && call.endsWith("}") -> {
-            val condition = parseExpression(call.substringAfter("filter{").dropLast(1)) ?: throw ParseException()
+            val condition = parseExpression(call.substringAfter("filter{").dropLast(1))
             if (condition !is Bool) throw TypeException()
             FilterCall(condition)
         }
@@ -42,17 +42,17 @@ fun parseConstantExpression(cExpr: String): ConstantExpression {
 }
 
 fun parseBinaryOperation(binExpr: String): BinaryOperator {
-    val ops = "-<>=&|*+"
-    val constExpr = { sg: String -> "$sg[0-9]*" }
+    val ops = """[-<>=&|*+]"""
+    val constExpr = """-?\d+""".toRegex()
 
-    val leftElem = """\(element([$ops])(.*)\)""".toRegex()
+    val leftElem = """\(element($ops)(.*)\)""".toRegex()
     var matchResult = leftElem.matchEntire(binExpr)
     if (matchResult != null) {
         val (op, r) = matchResult.destructured
         val rExpr = parseExpression(r)
         return safeCreate(op, Element, rExpr)
     }
-    val rightElem = """\((.*)([$ops])element\)""".toRegex()
+    val rightElem = """\((.*)($ops)element\)""".toRegex()
     matchResult = rightElem.matchEntire(binExpr)
     if (matchResult != null) {
         val (l, op) = matchResult.destructured
@@ -60,7 +60,7 @@ fun parseBinaryOperation(binExpr: String): BinaryOperator {
         return safeCreate(op, lExpr, Element)
     }
 
-    val twoOperations = """\((\(.*\))([$ops])(\(.*\))\)""".toRegex()
+    val twoOperations = """\((\(.*\))($ops)(\(.*\))\)""".toRegex()
     matchResult = twoOperations.matchEntire(binExpr)
     if (matchResult != null) {
         val (l, op, r) = matchResult.destructured
@@ -69,29 +69,24 @@ fun parseBinaryOperation(binExpr: String): BinaryOperator {
         return safeCreate(op, lExpr, rExpr)
     }
 
-    val rightConstant = { sg: String -> """\((\(.*\))([$ops])(${constExpr(sg)})\)""".toRegex() }
-    matchResult = rightConstant("-").matchEntire(binExpr) ?: rightConstant("").matchEntire(binExpr)
+    val rightConstant = """\((\(.*\))($ops)(${constExpr})\)""".toRegex()
+    matchResult = rightConstant.matchEntire(binExpr)
     if (matchResult != null) {
         val (l, op, r) = matchResult.destructured
         val lExpr = parseExpression(l)
         return safeCreate(op, lExpr, ConstantExpression(r))
     }
 
-    val leftConstant = { sg: String -> """\((${constExpr(sg)})([$ops])(\(.*\))\)""".toRegex() }
-    matchResult = leftConstant("-").matchEntire(binExpr) ?: leftConstant("").matchEntire(binExpr)
+    val leftConstant = """\((${constExpr})($ops)(\(.*\))\)""".toRegex()
+    matchResult = leftConstant.matchEntire(binExpr)
     if (matchResult != null) {
         val (l, op, r) = matchResult.destructured
         val rExpr = parseExpression(r)
         return safeCreate(op, ConstantExpression(l), rExpr)
     }
 
-    val twoConstants =
-        { sgL: String, sgR: String -> """\((${constExpr(sgL)})([$ops])(${constExpr(sgR)})\)""".toRegex() }
-    matchResult =
-        twoConstants("-", "-").matchEntire(binExpr) ?: twoConstants("", "-").matchEntire(binExpr) ?: twoConstants(
-            "-",
-            ""
-        ).matchEntire(binExpr) ?: twoConstants("", "").matchEntire(binExpr)
+    val twoConstants = """\((${constExpr})($ops)(${constExpr})\)""".toRegex()
+    matchResult = twoConstants.matchEntire(binExpr)
     return if (matchResult != null) {
         val (l, op, r) = matchResult.destructured
         val lExpr = parseExpression(l)

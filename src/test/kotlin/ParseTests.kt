484a3d0106ba0a4d568e6d1542373b5a6ffc4638
Maria F <myannyax@gmail.com>
2020-04-01 20:58:17 +0300

    test: add toString tests

--- src/test/kotlin/ParseTests.kt
+++ src/test/kotlin/ParseTests.kt
@@ -185,4 +185,39 @@ class ParseTests {
             ).reordered()
         )
     }
+
+    @Test
+    fun `test toString`() {
+        assertEquals(Plus(ConstantExpression("1"), ConstantExpression("2")).toString(), "(1+2)")
+        assertEquals(Mult(ConstantExpression("1"), ConstantExpression("2")).toString(), "(1*2)")
+        assertEquals(Minus(ConstantExpression("1"), ConstantExpression("2")).toString(), "(1-2)")
+        assertEquals(Gt(ConstantExpression("1"), ConstantExpression("2")).toString(), "(1>2)")
+        assertEquals(Lt(ConstantExpression("1"), ConstantExpression("2")).toString(), "(1<2)")
+        assertEquals(Eq(ConstantExpression("1"), ConstantExpression("2")).toString(), "(1=2)")
+        assertEquals(And(TRUE, TRUE).toString(), "((1=1)&(1=1))")
+        assertEquals(Or(TRUE, TRUE).toString(), "((1=1)|(1=1))")
+
+        assertEquals(
+            MapCall(
+                Plus(
+                    Mult(Element, Element),
+                    Plus(Mult(Element, ConstantExpression("20")), ConstantExpression("100"))
+                )
+            ).toString(), "map{((element*element)+((element*20)+100))}"
+        )
+        assertEquals(
+            FilterCall(Gt(Plus(Element, ConstantExpression("10")), ConstantExpression("10"))).toString(),
+            "filter{((element+10)>10)}"
+        )
+        assertEquals(
+            CallChain(
+                listOf(
+                    MapCall(Plus(Element, ConstantExpression("10"))),
+                    FilterCall(Gt(Element, ConstantExpression("10"))),
+                    MapCall(Mult(Element, Element))
+                )
+            ).toString(), "map{(element+10)}%>%filter{(element>10)}%>%map{(element*element)}"
+        )
+
+    }
 }
